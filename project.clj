85742940bb6159fc19dddb201fd20e508ebf1eee
Milinda Pathirage <milinda.pathirage@gmail.com>
2014-04-05 11:07:02 -0400

    first commit without any code.

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject org.pathirage.clojure.magic "0.1.0-SNAPSHOT"
+  :description "Microbenchmarks and fun things related to Clojure"
+  :url "https://github.com/milinda/clojure-magic"
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :source-paths      ["src/clj"]
+  :java-source-paths ["src/java"]
+  :dependencies [[org.clojure/clojure "1.5.1"]])
